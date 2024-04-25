f9beffe19f153de668c11fc3e9b970d27cdf80ff
Clayton Prickett <clayton.a.prickett@gmail.com>
2024-04-25 15:54:31 -0400

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 import { DashboardNav } from '@/components/dashboardNav';
 
-export default function DashboradLayout({
+export default function DashboardLayout({
 	children,
 }: {
 	children: ReactNode;
